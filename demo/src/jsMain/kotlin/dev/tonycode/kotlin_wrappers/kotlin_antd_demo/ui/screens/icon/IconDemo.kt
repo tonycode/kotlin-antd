f99bdcff648346e94790e0589f10ec65beac2402
Anton Vasilev <6095724+tonycode@users.noreply.github.com>
2023-11-05 21:32:39 +0200

    feat: Update IconDemo

--- demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_antd_demo/ui/screens/icon/IconDemo.kt
+++ demo/src/jsMain/kotlin/dev/tonycode/kotlin_wrappers/kotlin_antd_demo/ui/screens/icon/IconDemo.kt
@@ -1,10 +1,14 @@
 package dev.tonycode.kotlin_wrappers.kotlin_antd_demo.ui.screens.icon
 
+import antd.CheckCircleTwoTone
 import antd.Direction
+import antd.HeartTwoTone
 import antd.HomeOutlined
 import antd.LoadingOutlined
 import antd.SettingFilled
 import antd.Size
+import antd.SmileOutlined
+import antd.SmileTwoTone
 import antd.Space
 import antd.StarFilled
 import antd.StarOutlined
@@ -13,6 +17,7 @@ import antd.SyncOutlined
 import react.FC
 import react.Props
 import web.cssom.Color
+import web.cssom.NamedColor
 
 
 val IconDemo = FC<Props>("IconDemo") {
@@ -21,19 +26,24 @@ val IconDemo = FC<Props>("IconDemo") {
         direction = Direction.vertical
         size = Size.middle
 
-        // row 1
         Space {
             HomeOutlined()
             SettingFilled()
             SyncOutlined { spin = true }
+            SmileOutlined { rotate = 180 }
             LoadingOutlined()
         }
 
-        // row 2
+        Space {
+            SmileTwoTone()
+            HeartTwoTone { twoToneColor = Color("#eb2f96") }
+            CheckCircleTwoTone { twoToneColor = NamedColor.orange }
+        }
+
         Space {
             StarOutlined()
             StarFilled()
-            StarTwoTone { twoToneColor = Color("#eb2f96") }
+            StarTwoTone { twoToneColor = NamedColor.red }
         }
     }
 
